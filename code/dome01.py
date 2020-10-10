b7b78e6b0d33675770285155fe99dd736c3b9716
rli42 <2950063526@11.com>
2020-10-10 22:22:58 +0800

    代码练习

---
+++ code/dome01.py
@@ -0,0 +1,17 @@
+#! usr/bin/python
+# -*- coding:utf-8 -*-
+# lis = ['a', 'b', 'c', 'e']
+# str1 = '-'.join(lis)
+# print(str1)
+# for i in lis:
+#     print(i, sep="-", end="")
+
+# 格式化字符串，吧列表变为字符串使用字符串方法 join()以 ’-‘连接 注意：列表内类型需要是字符串
+# lis = ['2020', '10', '10', '21', '43']
+# while True:
+#     mi_input = input("请输入(输入0结束)：")
+#     if mi_input == "0":
+#         print(':'.join(lis))
+#         break
+#     else:
+#         lis.append(mi_input)
