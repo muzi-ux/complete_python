387deb4a51f17f37cf1606020023eb546a3ef8f0
rli42 <2950063526@11.com>
2020-10-15 07:57:03 +0800

    代码练习

--- code/dome01.py
+++ code/dome01.py
@@ -17,16 +17,39 @@
 #         lis.append(mi_input)
 
 # print 打印语句
-print('hello word !')
+# print('hello word !')
 # print 打印语句结尾默认换行
 # end='' 打印语句的结尾不进行换行，
-print('hello', end=' ')  # 打印语句以空格结束，end='xxx' 引号内部所放的内容会出现在该打印语句的最后
-print('word !', end='\n')
+# print('hello', end=' ')  # 打印语句以空格结束，end='xxx' 引号内部所放的内容会出现在该打印语句的最后
+# print('word !', end='\n')
+#
+# name = '木子'
+# age = 18
+# print(name, age, sep='-')  # sep='xx' 引号内的符号会作为打印语句的连接符
+#
+# name = '张二狗'
+# age = 18
+# print(name, age, sep="-")
 
-name = '木子'
-age = 18
-print(name, age, sep='-')  # sep='xx' 引号内的符号会作为打印语句的连接符
+import pymssql
 
-name = '张二狗'
-age = 18
-print(name, age, sep="-")
+# conn = pymssql.connect('DESKTOP-SPKTIB1\SQLEXPRESS', 'sa', '123456', 'student')
+# # if conn:
+# #     print('链接成功')
+# # conn.close()
+
+
+# def conn():
+#     connect = pymssql.connect(server=r'DESKTOP-SPKTIB1\SQLEXPRESS', user=r'sa', password='123456', database='mi')
+#     if connect:
+#         print('链接成功！')
+#     return connect
+
+
+# login_timeout=10,
+# charset="UTF-8"
+# serve=r'SQLEXPRESS',
+# conn()
+
+age = input("请输入年龄")
+print(type(age))
