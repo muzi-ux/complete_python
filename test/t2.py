b921c074d02fdea8a383bd765a9193d44157e21a
rli42 <2950063526@11.com>
2020-11-05 22:42:59 +0800

    学习

--- test/t2.py
+++ test/t2.py
@@ -1,12 +1,1 @@
-a = 100
 
-
-def main():
-    global a  # global 关键字声明这里的a是全局变量
-    a = 10
-    s = 9
-    print(a, s)  # 10
-
-
-print(a)  # 10
-print("全局变量有{} 局部变量有{}".format(globals(), locals()))
