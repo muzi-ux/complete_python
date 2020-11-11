97d0a126e1b155130e642b772b84456d609722d2
rli42 <2950063526@11.com>
2020-11-11 22:24:27 +0800

    学习

--- test/t2.py
+++ test/t2.py
@@ -1,1 +1,16 @@
+# def fibonacci(i):
+#     if i == 2 or i == 1:
+#         return 1
+#     return fibonacci(i - 2) + fibonacci(i - 1)
+#
+#
+# print(fibonacci(20))
 
+
+lis = []
+for i in range(25):
+    lis.append(50 + i + 1)
+
+print(lis)
+print(len(lis))
+print(sum(lis))
