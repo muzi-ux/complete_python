7a1ba699032fda831e442f379ab6f656bb67172b
rli42 <2950063526@11.com>
2020-11-12 12:35:21 +0800

    学习

--- test/t2.py
+++ test/t2.py
@@ -7,10 +7,35 @@
 # print(fibonacci(20))
 
 
-lis = []
-for i in range(25):
-    lis.append(50 + i + 1)
+# lis = []
+# for i in range(25):
+#     lis.append(50 + i + 1)
+#
+# print(lis)
+# print(len(lis))
+# print(sum(lis))
 
-print(lis)
-print(len(lis))
-print(sum(lis))
+
+# def my_sum(m, b):
+#     return m + b
+#
+#
+# def sum1(z, n):
+#     return my_sum(z, n)
+#
+#
+# print(sum1(1, 4))
+
+def external():
+    print('我是外部函数')
+
+    def internal():  # 在函数外部是没有办法调用函数呢部的变量的
+        print('我是内部函数')
+
+    return internal
+
+
+external()  # 我是外部函数
+
+internal = external()
+internal()  # 我是内部函数
